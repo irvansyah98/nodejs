ad3ea010f759f2d95ff75c7cf40b7f54152b5321
iervan <iervanfirdiansyah1998@gmail.com>
2022-07-18 09:37:35 +0700

    update package

--- npm/package.js
+++ npm/package.js
@@ -1,6 +1,10 @@
 import { sayHello, sum } from "npm-library-by-irvan";
+import { max, min } from "npm-library-by-irvan/number";
 
 
 console.info(sayHello("iervan"));
 
 console.info(sum([10,10,10,10,10]));
+
+console.info(min(10,20));
+console.info(max(10,20));
